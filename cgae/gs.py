fixed a pretty serious gumble softmax bug

cgae/gs.py
@@ -9,12 +9,12 @@ def sample_gumbel(shape, dtype=None, device=None, eps=1e-10):
 
 def sample_gumbel_softmax(logits, temperature, dim=-1, dtype=None, device=None):
     y = logits + sample_gumbel(logits.size(), dtype=dtype, device=device)
-    return torch.nn.functional.softmax(y / temperature, dim=-dim)
+    return torch.nn.functional.softmax(y / temperature, dim=dim)
 
 
 def gumbel_softmax(logits, temperature, dim=-1, dtype=None, device=None):
     """Sample a gumbel softmax distribution in dim = -1. Returns gumble, straight-through gumble."""
     y = sample_gumbel_softmax(logits, temperature, dim=dim, dtype=dtype, device=device)
-    st_y = torch.zeros(y.shape, dtype=dtype, device=device).scatter_(-1, y.argmax(-1).unsqueeze(-1), 1.0)
+    st_y = torch.zeros(y.shape, dtype=dtype, device=device).scatter_(dim, y.argmax(dim).unsqueeze(dim), 1.0)
     st_y = (st_y - y).detach() + y
     return y, st_y

otp_equi.py
@@ -110,7 +110,7 @@ def execute(args):
 
             # Auto encoder
             logits = encoder(feat, geo)
-            cg_assign, st_cg_assign = gumbel_softmax(logits, temp_sched[epoch], dim=1,
+            cg_assign, st_cg_assign = gumbel_softmax(logits, temp_sched[epoch],
                                                      dtype=args.precision, device=args.device)
             E = cg_assign / cg_assign.sum(1).unsqueeze(1)
             cg_xyz = torch.einsum('zij,zik->zjk', E, geo)
@@ -144,7 +144,8 @@ def execute(args):
                 'epoch': epoch,
                 'step': step,
                 'temp': temp_sched[epoch].item(),
-                'gumble': gumbel_softmax(logits, temp_sched[epoch], device=args.device),
+                'gumble': cg_assign,
+                'st_gumble': st_cg_assign,
                 'batch': batch.item(),
                 'cg_xyz': cg_xyz,
                 'pred_sph': pred_sph,